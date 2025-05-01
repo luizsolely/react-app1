d6e547a17407569380fcd53f6d6b81265ffdf000
Luiz Felipe <solelydev1@gmail.com>
2025-05-01 18:11:37 -0300

    adjust

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,9 @@ const Projects = () => {
 
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h1 className="my-20 text-center text-4xl">Projects</h1>
+      <h1 className="my-20 text-center text-4xl">
+        {language === 0 ? 'Projetos' : 'Projects'}
+      </h1>
       <div>
         {PROJECTS.map((project, index) => (
         <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
@@ -26,14 +28,14 @@ const Projects = () => {
               <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium'>{tech}</span>
             ))}
             <a
-  href={project.github_link}
-  target="_blank"
-  rel="noopener noreferrer"
-  className="inline-flex items-center text-sm text--400 hover:underline mt-2"
->
-  <FaGithub className="mr-1" />
-  View on GitHub
-</a>
+              href={project.github_link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center text-sm text--400 hover:underline mt-2"
+            >
+              <FaGithub className="mr-1" />
+                {language === 0 ? 'Ver no GitHub' : 'View on GitHub'}
+          </a>
           </div>
         </div>
       ))}
